6f67a47e69177ecd44a670f1e4482227db848a8d
Wreulicke <masaya2dies@gmail.com>
2017-02-01 07:29:04 +0900

    [WIP]add collector

--- src/processor.js
+++ src/processor.js
@@ -6,16 +6,33 @@ const github = require("remark-github")
 const remark2rehype = require("remark-rehype")
 const highlight = require("rehype-highlight")
 const rehype2react = require("rehype-react")
-const collector = require("./collector")
+const {collector} = require("./collector")
+const Collector = collector.Collector
 const React = require("react")
-
+const Immutable = require("immutable")
+let map = Immutable.List.of()
+let head = -1
+const descriptor = (parent) => {
+  parent.children.forEach((node) => {
+    if (node.type == "heading") {
+      head++
+      map = map.push(Immutable.List.of(node))
+    } else
+      map = map.insert(head, map.get(head).push(node))
+  })
+}
 module.exports = unified()
   .use(markdown)
   .use(toc)
   .use(github, {
     repository: "https://github.com/rhysd/rehype-react"
   })
-  .use(collector)
+  .use(collector, {
+    collector: new Collector((node) => {
+      descriptor(node)
+      console.log(map.toJS())
+    })
+  })
   .use(remark2rehype)
   .use(highlight)
   .use(rehype2react, {
